ffbfa7923eea21be2062f085485bc9a905b534e4
Henry Gebhardt <hsggebhardt@psu.edu>
2014-01-31 12:07:53 -0500

    hw3: some of problem 2

--- hw3/2_io.jl
+++ hw3/2_io.jl
@@ -36,3 +36,36 @@ println("Reading2^20: ", time, " seconds")
 # With 1024*1024 floats in an array, there is much less a difference between
 # subsequent readings. Presumably the array is now large enough to not be
 # cached in any way.
+
+
+# 2d
+function writebin(filename::String, v::Array)
+    # ignore error handling
+    stream = open(filename, "w")
+    write(stream, v)
+    close(stream)
+end
+function readbin!(filename::String, v::Array)
+    stream = open(filename, "r")
+    v[:] = read(stream, typeof(v))
+    close(stream)
+    return v
+end
+
+time = @elapsed writebin("wozniak.bin", v)
+println("Writing2^20 in binary: ", time, " seconds")
+time = @elapsed readbin!("wozniak.bin", v)
+println("Reading2^20 in binary: ", time, " seconds")
+time = @elapsed readbin!("wozniak.bin", v)
+println("Reading2^20 in binary: ", time, " seconds")
+time = @elapsed readbin!("wozniak.bin", v)
+println("Reading2^20 in binary: ", time, " seconds")
+
+println("ASCII file sie: ", stat("torvalds.dat").size / 2^20, " MB")
+println("Binary file sie: ", stat("wozniak.dat").size / 2^20, " MB")
+
+
+# 2e
+using HDF5, JLD
+@save "holy.jld" v
+@load "holy.jld"
