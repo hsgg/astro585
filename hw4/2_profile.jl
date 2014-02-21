d64d216d7ba383278cd2e8ef1e64b13fd75b204b
The Maxwell-Jüttner Distribution <the.maxwell.juettner.distribution@gmail.com>
2014-02-20 21:23:36 -0500

    hw4/2_profile.jl: simplify code

--- hw4/2_profile.jl
+++ hw4/2_profile.jl
@@ -1,38 +1,40 @@
 #!/usr/bin/env julia
 
-function make_data(Nobs::Int)
-    srand(42)
-    z = zeros(Nobs)
-    sigma = 2. * ones(Nobs)
-    y = z + sigma .* randn(Nobs)
-    return y, sigma, z
+function log_deMoivre(z, y, sigma)
+    tmp = y - z
+    tmp /= sigma
+    tmp = tmp^2
+    tmp *= -0.5
+    tmp2 = sqrt(2 * pi)
+    tmp2 *= sigma
+    tmp2 = log(tmp2)
+    tmp -= tmp2
+    return tmp
 end
-
-
-log_deMoivre(z, y, sigma) = -0.5 * ((y-z)/sigma)^2 - log(sqrt(2*pi) * sigma)
 
 function log_likelihood(log_pdf, y::Array, sigma::Array, z::Array)
     n = length(y)
     @assert n == length(sigma)
     @assert n == length(z)
-    sum = @profile zero(y[1])
+    sum = zero(y[1])
     for i in 1:n
-        @profile sum += log_pdf(y[i], z[i], sigma[i])
+        a = y[i]
+        b = z[i]
+        c = sigma[i]
+        sum += log_pdf(a, b, c)
     end
-    
     return sum
 end
 
-function run_m_times(func, log_pdf, Nobs::Int, M::Int)
-    y, sigma, z = make_data(Nobs)
-    sum = 0.0
-    for i in 1:M
-        sum += func(log_pdf, y, sigma, z)
-    end
-    return sum # return something
-end
+# make data:
+Nobs = 100
+srand(42)
+z = zeros(Nobs)
+sigma = 2. * ones(Nobs)
+y = z + sigma .* randn(Nobs)
 
-println("Time taken: ", @elapsed run_m_times(log_likelihood, log_deMoivre, iround(3e3), 10^4))
-
-Profile.print()
+# profile:
 Profile.clear()
+time = @elapsed @profile (for i in 1:10^5; log_likelihood(log_deMoivre, y, sigma, z); end)
+println("Time elapsed: ", time, " seconds")
+Profile.print()
