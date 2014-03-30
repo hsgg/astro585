5435ec2861b57095161ddd649091002ac12395db
The Maxwell-Jüttner Distribution <the.maxwell.juettner.distribution@gmail.com>
2014-03-30 00:43:30 -0400

    hw7/soln.tex: wrap it up

--- hw7/normal_pdf_gpu.cu
+++ hw7/normal_pdf_gpu.cu
@@ -8,11 +8,13 @@ extern "C"   // ensure function name to be left alone
         if(i<n)  y[i] = exp(-0.5*x[i]*x[i])*rsqrt(2.0*M_PI);
     }
 
-    __global__ void sum_gpu(double *y, double *sumptr, unsigned int n, unsigned int n_subsums, unsigned int percore)
+    __global__ void sum_gpu(double *y, double *sumptr, unsigned int n,
+            unsigned int n_subsums, unsigned int percore)
     {
         // assumes a 2-d grid of 1-d blocks
-        unsigned int i = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x + threadIdx.x;
-        unsigned int j = i * percore;  // first element that this thread will take care of
+        unsigned int i = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x
+            + threadIdx.x;
+        unsigned int j = i * percore; // first element that this thread will take care of
         unsigned int k;
 
         if (i >= n_subsums)
@@ -26,6 +28,7 @@ extern "C"   // ensure function name to be left alone
     }
 
     __global__ void get_sum_gpu(double *y, double *sumptr)
+        // copy the result into a smaller array
     {
         sumptr[0] = y[0];
     }
